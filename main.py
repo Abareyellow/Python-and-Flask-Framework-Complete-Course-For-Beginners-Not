a1a13e950770e52e33550d14034b8f773cbee17b
KatieS. <30733981+Abareyellow@users.noreply.github.com>
2024-05-20 13:01:10 +0000

    Lesson Complete

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return 'Hello Katie!'
+@app.route('/hello/<name>')
+def index(name):
+    return 'Hello %s' % name
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=5000)
