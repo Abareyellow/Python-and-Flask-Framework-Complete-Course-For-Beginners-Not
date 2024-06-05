6af0afaace0d51a94c60ce4aa815858f770e385d
KatieS. <30733981+Abareyellow@users.noreply.github.com>
2024-06-05 12:46:26 +0000

    Done

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template, request, make_response
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'Hello Katie!'
+    return render_template('setcookie.html')
+
+@app.route('/getcookie')
+def getcookie():
+    name = request.cookies.get('userID')
+    return '<h1>Welcome {}</h1>'.format(name)
+
+@app.route('/setcookie', methods = ['POST', 'GET'])
+def setcookie():
+    print(request.method)
+    if request.method == 'POST':
+        user = request.form['nm']
+        resp = make_response(render_template('readcookie.html'))
+        resp.set_cookie('userID', user)
+        return resp
+    else:
+        return render_template('setcookie.html')
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=5000)
+  app.run(host='0.0.0.0', port=5000, debug = True)
   # app.run(debug=True)
